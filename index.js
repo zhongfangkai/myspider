af4d82541831889da14a74c3b0964ce2392cec0d
zhongfangkai <1275154889@qq.com>
2019-08-28 17:48:06 +0800

    spider test

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+// 引入 puppeteer 模块
 const puppeteer = require('puppeteer');
+// 引入 cheerio 模块
+const cheerio = require('cheerio')
+// 引入fs模块
+var fs = require('fs'); 
+
+// sleep
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 (async () => {
     const browser = await puppeteer.launch({
-        // headless: false,
+        headless: false,
+        devtools: true,
         defaultViewport: {
             width: 1600,
             height: 900
@@ -26,18 +37,52 @@ const puppeteer = require('puppeteer');
 
     console.log('page content => ', content);
 
-    await page.screenshot({
-        path: 'resource/sunnoe.png'
-    });
-
-    console.log('page screenshot is done!');
-
-    await page.pdf({
-        path: 'resource/sunnoe.pdf',
-        format: 'A6'
-    });
+    const $ = cheerio.load(content);
     
-    console.log('page pdf is done!');
+    var list = $('.stream-list .stream-item');
+
+    var data = [];
+    var item;
+
+    for(var i=0;i<list.length;i++){
+        item = $(list[i]);
+        data[i] = {};
+        data[i]['title'] = item.find('.doc_node_title').text().trim();
+        data[i]['content'] = item.find('.doc_node_content').text().trim();
+        // data[i]['info'] = item.find('.side_unit_info').html().trim();
+    }
+    
+    console.log(data);
+
+    fs.writeFileSync('./data.json',JSON.stringify(data),function(error){
+        if(error){
+            console.log('写入失败');
+        }else{
+            console.log('写入成功');
+        }
+    });
+
+    // fs.close();
+
+    // console.log(list);
+    // await page.screenshot({
+    //     path: 'resource/sunnoe.png'
+    // });
+
+    // console.log('page screenshot is done!');
+
+    // await page.pdf({
+    //     path: 'resource/sunnoe.pdf',
+    //     format: 'A6'
+    // });
+    
+    // console.log('page pdf is done!');
+
+    console.log('sleep 5 second');
+
+    await sleep(5000);
+
+    console.log('sleep 5 second done');
 
     await browser.close();
 })();
